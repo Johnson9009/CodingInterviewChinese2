68529247396bd8e52fa85e8c7d9c634f70ed5708
Johnson Zhang <Johnson9009@163.com>
2018-06-03 13:31:56 +0800

    Practice O(nlogn) Partition Solution of 40_KLeastNumbers

--- 40_KLeastNumbers/KLeastNumbers.cpp
+++ 40_KLeastNumbers/KLeastNumbers.cpp
@@ -19,176 +19,155 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 #include <cstdio>
 #include "../Utilities/Array.h"
 
+#include <functional>
+#include <iostream>
 #include <set>
 #include <vector>
-#include <iostream>
-#include <functional>
 
 using namespace std;
 
 // ====================方法1====================
-void GetLeastNumbers_Solution1(int* input, int n, int* output, int k)
-{
-    if(input == nullptr || output == nullptr || k > n || n <= 0 || k <= 0)
-        return;
-
-    int start = 0;
-    int end = n - 1;
-    int index = Partition(input, n, start, end);
-    while(index != k - 1)
-    {
-        if(index > k - 1)
-        {
-            end = index - 1;
-            index = Partition(input, n, start, end);
-        }
-        else
-        {
-            start = index + 1;
-            index = Partition(input, n, start, end);
-        }
+void GetLeastNumbers_Solution1(int* data, int length, int* output, int k) {
+  if ((data == nullptr) || (output == nullptr) || (k <= 0) || (length < k)) {
+    throw std::invalid_argument("");
+  }
+  int begin = 0;
+  int end = length;
+  while ((begin + 1) < end) {
+    int pivot_idx = Partition(data, begin, end);
+    if ((pivot_idx + 1) == k) {
+      break;
+    } else if ((pivot_idx + 1) < k) {
+      begin = pivot_idx + 1;
+    } else {
+      end = pivot_idx;
     }
-
-    for(int i = 0; i < k; ++i)
-        output[i] = input[i];
+  }
+  for (int i = 0; i < k; ++i) output[i] = data[i];
+  return;
 }
 
 // ====================方法2====================
-typedef multiset<int, std::greater<int> >            intSet;
-typedef multiset<int, std::greater<int> >::iterator  setIterator;
+typedef multiset<int, std::greater<int> > intSet;
+typedef multiset<int, std::greater<int> >::iterator setIterator;
 
-void GetLeastNumbers_Solution2(const vector<int>& data, intSet& leastNumbers, int k)
-{
-    leastNumbers.clear();
+void GetLeastNumbers_Solution2(const vector<int>& data, intSet& leastNumbers, int k) {
+  leastNumbers.clear();
 
-    if(k < 1 || data.size() < k)
-        return;
+  if (k < 1 || data.size() < k) return;
 
-    vector<int>::const_iterator iter = data.begin();
-    for(; iter != data.end(); ++ iter)
-    {
-        if((leastNumbers.size()) < k)
-            leastNumbers.insert(*iter);
+  vector<int>::const_iterator iter = data.begin();
+  for (; iter != data.end(); ++iter) {
+    if ((leastNumbers.size()) < k)
+      leastNumbers.insert(*iter);
 
-        else
-        {
-            setIterator iterGreatest = leastNumbers.begin();
+    else {
+      setIterator iterGreatest = leastNumbers.begin();
 
-            if(*iter < *(leastNumbers.begin()))
-            {
-                leastNumbers.erase(iterGreatest);
-                leastNumbers.insert(*iter);
-            }
-        }
+      if (*iter < *(leastNumbers.begin())) {
+        leastNumbers.erase(iterGreatest);
+        leastNumbers.insert(*iter);
+      }
     }
+  }
 }
 
 // ====================测试代码====================
-void Test(char* testName, int* data, int n, int* expectedResult, int k)
-{
-    if(testName != nullptr)
-        printf("%s begins: \n", testName);
+void Test(char const* testName, int* data, int n, int* expectedResult, int k) {
+  if (testName != nullptr) printf("%s begins: \n", testName);
 
-    vector<int> vectorData;
-    for(int i = 0; i < n; ++ i)
-        vectorData.push_back(data[i]);
+  vector<int> vectorData;
+  for (int i = 0; i < n; ++i) vectorData.push_back(data[i]);
 
-    if(expectedResult == nullptr)
-        printf("The input is invalid, we don't expect any result.\n");
-    else
-    {
-        printf("Expected result: \n");
-        for(int i = 0; i < k; ++ i)
-            printf("%d\t", expectedResult[i]);
-        printf("\n");
-    }
+  if (expectedResult == nullptr)
+    printf("The input is invalid, we don't expect any result.\n");
+  else {
+    printf("Expected result: \n");
+    for (int i = 0; i < k; ++i) printf("%d\t", expectedResult[i]);
+    printf("\n");
+  }
 
-    printf("Result for solution1:\n");
-    int* output = new int[k];
+  printf("Result for solution1:\n");
+  int* output = new int[k];
+  try {
     GetLeastNumbers_Solution1(data, n, output, k);
-    if(expectedResult != nullptr)
-    {
-        for(int i = 0; i < k; ++ i)
-            printf("%d\t", output[i]);
-        printf("\n");
+    for (int i = 0; i < k; ++i) printf("%d\t", output[i]);
+    printf("\n");
+  } catch (std::invalid_argument const& ia) {
+    if (expectedResult == nullptr) {
+      printf("Passed!\n");
+    } else {
+      printf("Failed!\n");
     }
+  }
 
-    delete[] output;
+  delete[] output;
 
-    printf("Result for solution2:\n");
-    intSet leastNumbers;
-    GetLeastNumbers_Solution2(vectorData, leastNumbers, k);
-    printf("The actual output numbers are:\n");
-    for(setIterator iter = leastNumbers.begin(); iter != leastNumbers.end(); ++iter)
-        printf("%d\t", *iter);
-    printf("\n\n");
+  printf("Result for solution2:\n");
+  intSet leastNumbers;
+  GetLeastNumbers_Solution2(vectorData, leastNumbers, k);
+  printf("The actual output numbers are:\n");
+  for (setIterator iter = leastNumbers.begin(); iter != leastNumbers.end(); ++iter)
+    printf("%d\t", *iter);
+  printf("\n\n");
 }
 
 // k小于数组的长度
-void Test1()
-{
-    int data[] = {4, 5, 1, 6, 2, 7, 3, 8};
-    int expected[] = {1, 2, 3, 4};
-    Test("Test1", data, sizeof(data) / sizeof(int), expected, sizeof(expected) / sizeof(int));
+void Test1() {
+  int data[] = {4, 5, 1, 6, 2, 7, 3, 8};
+  int expected[] = {1, 2, 3, 4};
+  Test("Test1", data, sizeof(data) / sizeof(int), expected, sizeof(expected) / sizeof(int));
 }
 
 // k等于数组的长度
-void Test2()
-{
-    int data[] = {4, 5, 1, 6, 2, 7, 3, 8};
-    int expected[] = {1, 2, 3, 4, 5, 6, 7, 8};
-    Test("Test2", data, sizeof(data) / sizeof(int), expected, sizeof(expected) / sizeof(int));
+void Test2() {
+  int data[] = {4, 5, 1, 6, 2, 7, 3, 8};
+  int expected[] = {1, 2, 3, 4, 5, 6, 7, 8};
+  Test("Test2", data, sizeof(data) / sizeof(int), expected, sizeof(expected) / sizeof(int));
 }
 
 // k大于数组的长度
-void Test3()
-{
-    int data[] = {4, 5, 1, 6, 2, 7, 3, 8};
-    int* expected = nullptr;
-    Test("Test3", data, sizeof(data) / sizeof(int), expected, 10);
+void Test3() {
+  int data[] = {4, 5, 1, 6, 2, 7, 3, 8};
+  int* expected = nullptr;
+  Test("Test3", data, sizeof(data) / sizeof(int), expected, 10);
 }
 
 // k等于1
-void Test4()
-{
-    int data[] = {4, 5, 1, 6, 2, 7, 3, 8};
-    int expected[] = {1};
-    Test("Test4", data, sizeof(data) / sizeof(int), expected, sizeof(expected) / sizeof(int));
+void Test4() {
+  int data[] = {4, 5, 1, 6, 2, 7, 3, 8};
+  int expected[] = {1};
+  Test("Test4", data, sizeof(data) / sizeof(int), expected, sizeof(expected) / sizeof(int));
 }
 
 // k等于0
-void Test5()
-{
-    int data[] = {4, 5, 1, 6, 2, 7, 3, 8};
-    int* expected = nullptr;
-    Test("Test5", data, sizeof(data) / sizeof(int), expected, 0);
+void Test5() {
+  int data[] = {4, 5, 1, 6, 2, 7, 3, 8};
+  int* expected = nullptr;
+  Test("Test5", data, sizeof(data) / sizeof(int), expected, 0);
 }
 
 // 数组中有相同的数字
-void Test6()
-{
-    int data[] = {4, 5, 1, 6, 2, 7, 2, 8};
-    int expected[] = {1, 2};
-    Test("Test6", data, sizeof(data) / sizeof(int), expected, sizeof(expected) / sizeof(int));
+void Test6() {
+  int data[] = {4, 5, 1, 6, 2, 7, 2, 8};
+  int expected[] = {1, 2};
+  Test("Test6", data, sizeof(data) / sizeof(int), expected, sizeof(expected) / sizeof(int));
 }
 
 // 输入空指针
-void Test7()
-{
-    int* expected = nullptr;
-    Test("Test7", nullptr, 0, expected, 0);
+void Test7() {
+  int* expected = nullptr;
+  Test("Test7", nullptr, 0, expected, 0);
 }
 
-int main(int argc, char* argv[])
-{
-    Test1();
-    Test2();
-    Test3();
-    Test4();
-    Test5();
-    Test6();
-    Test7();
+int main(int argc, char* argv[]) {
+  Test1();
+  Test2();
+  Test3();
+  Test4();
+  Test5();
+  Test6();
+  Test7();
 
-    return 0;
+  return 0;
 }
-

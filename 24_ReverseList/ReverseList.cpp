3676704632bc46432195dcd3b0c516b549387422
Johnson Zhang <Johnson9009@163.com>
2018-06-04 16:17:45 +0800

    Practice 24_ReverseList

--- 24_ReverseList/ReverseList.cpp
+++ 24_ReverseList/ReverseList.cpp
@@ -19,82 +19,69 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 #include <cstdio>
 #include "../Utilities/List.h"
 
-ListNode* ReverseList(ListNode* pHead)
-{
-    ListNode* pReversedHead = nullptr;
-    ListNode* pNode = pHead;
-    ListNode* pPrev = nullptr;
-    while(pNode != nullptr)
-    {
-        ListNode* pNext = pNode->m_pNext;
-
-        if(pNext == nullptr)
-            pReversedHead = pNode;
-
-        pNode->m_pNext = pPrev;
-
-        pPrev = pNode;
-        pNode = pNext;
-    }
-
-    return pReversedHead;
+ListNode* ReverseList(ListNode* head) {
+  if ((head == nullptr) || (head->m_pNext == nullptr)) return head;
+  ListNode* cur_head = head;
+  ListNode* ahead = head;
+  ListNode* behind = head->m_pNext->m_pNext;
+  // This condition is very important.
+  while (ahead->m_pNext != nullptr) {
+    ahead->m_pNext->m_pNext = cur_head;
+    cur_head = ahead->m_pNext;
+    ahead->m_pNext = behind;
+    if (behind != nullptr) behind = behind->m_pNext;
+  }
+  return cur_head;
 }
-
 // ====================测试代码====================
-ListNode* Test(ListNode* pHead)
-{
-    printf("The original list is: \n");
-    PrintList(pHead);
+ListNode* Test(ListNode* pHead) {
+  printf("The original list is: \n");
+  PrintList(pHead);
 
-    ListNode* pReversedHead = ReverseList(pHead);
+  ListNode* pReversedHead = ReverseList(pHead);
 
-    printf("The reversed list is: \n");
-    PrintList(pReversedHead);
+  printf("The reversed list is: \n");
+  PrintList(pReversedHead);
 
-    return pReversedHead;
+  return pReversedHead;
 }
 
 // 输入的链表有多个结点
-void Test1()
-{
-    ListNode* pNode1 = CreateListNode(1);
-    ListNode* pNode2 = CreateListNode(2);
-    ListNode* pNode3 = CreateListNode(3);
-    ListNode* pNode4 = CreateListNode(4);
-    ListNode* pNode5 = CreateListNode(5);
+void Test1() {
+  ListNode* pNode1 = CreateListNode(1);
+  ListNode* pNode2 = CreateListNode(2);
+  ListNode* pNode3 = CreateListNode(3);
+  ListNode* pNode4 = CreateListNode(4);
+  ListNode* pNode5 = CreateListNode(5);
 
-    ConnectListNodes(pNode1, pNode2);
-    ConnectListNodes(pNode2, pNode3);
-    ConnectListNodes(pNode3, pNode4);
-    ConnectListNodes(pNode4, pNode5);
+  ConnectListNodes(pNode1, pNode2);
+  ConnectListNodes(pNode2, pNode3);
+  ConnectListNodes(pNode3, pNode4);
+  ConnectListNodes(pNode4, pNode5);
 
-    ListNode* pReversedHead = Test(pNode1);
+  ListNode* pReversedHead = Test(pNode1);
 
-    DestroyList(pReversedHead);
+  DestroyList(pReversedHead);
 }
 
 // 输入的链表只有一个结点
-void Test2()
-{
-    ListNode* pNode1 = CreateListNode(1);
+void Test2() {
+  ListNode* pNode1 = CreateListNode(1);
 
-    ListNode* pReversedHead = Test(pNode1);
+  ListNode* pReversedHead = Test(pNode1);
 
-    DestroyList(pReversedHead);
+  DestroyList(pReversedHead);
 }
 
 // 输入空链表
-void Test3()
-{
-    Test(nullptr);
+void Test3() {
+  Test(nullptr);
 }
 
-int main(int argc, char* argv[])
-{
-    Test1();
-    Test2();
-    Test3();
+int main(int argc, char* argv[]) {
+  Test1();
+  Test2();
+  Test3();
 
-    return 0;
+  return 0;
 }
-

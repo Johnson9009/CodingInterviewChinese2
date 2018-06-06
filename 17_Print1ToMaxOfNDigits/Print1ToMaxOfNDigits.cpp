c58b808ba38ca92a19ae5adcb350281b668aa15b
Johnson Zhang <Johnson9009@163.com>
2018-06-06 16:47:59 +0800

    Practice 17_Print1ToMaxOfNDigits

--- 17_Print1ToMaxOfNDigits/Print1ToMaxOfNDigits.cpp
+++ 17_Print1ToMaxOfNDigits/Print1ToMaxOfNDigits.cpp
@@ -8,149 +8,105 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 *******************************************************************/
 
 //==================================================================
-// 《剑指Offer――名企面试官精讲典型编程题》代码
-// 作者：何海涛
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+// 作者: 何海涛
 //==================================================================
 
-// 面试题17：打印1到最大的n位数
-// 题目：输入数字n，按顺序打印出从1最大的n位十进制数。比如输入3，则
-// 打印出1、2、3一直到最大的3位数即999。
+// 面试题17: 打印1到最大的n位数
+// 题目: 输入数字n, 按顺序打印出从1最大的n位十进制数. 比如输入3, 则打印出1, 2, 3一直到最大的3位数即
+// 999.
 
 #include <cstdio>
-#include <memory>
 #include <cstring>
+#include <memory>
 
-void PrintNumber(char* number);
-bool Increment(char* number);
-void Print1ToMaxOfNDigitsRecursively(char* number, int length, int index);
-
-// ====================方法一====================
-void Print1ToMaxOfNDigits_1(int n)
-{
-    if (n <= 0)
-        return;
-
-    char *number = new char[n + 1];
-    memset(number, '0', n);
-    number[n] = '\0';
-
-    while (!Increment(number))
-    {
-        PrintNumber(number);
-    }
-
-    delete[]number;
+void PrintNumber(char const* number_str) {
+  char const* cur_char = number_str;
+  while (*cur_char == '0') ++cur_char;
+  while (*cur_char != '\0') {
+    printf("%c", *cur_char);
+    ++cur_char;
+  }
+  printf("\t");
 }
 
-// 字符串number表示一个数字，在 number上增加1
-// 如果做加法溢出，则返回true；否则为false
-bool Increment(char* number)
-{
-    bool isOverflow = false;
-    int nTakeOver = 0;
-    int nLength = strlen(number);
+// ====================方法一====================
+bool IncreaseNumber(char* number_str) {
+  if (number_str == nullptr) throw std::invalid_argument("");
+  int length = strlen(number_str);
+  int carry = 1;
+  int cur_idx = length - 1;
+  while (carry != 0) {
+    int result = (number_str[cur_idx] - '0') + carry;
+    number_str[cur_idx] = '0' + result % 10;
+    carry = result / 10;
+    --cur_idx;
+    if ((cur_idx < 0) && (carry != 0)) return true;
+  }
+  return false;
+}
 
-    for (int i = nLength - 1; i >= 0; i--)
-    {
-        int nSum = number[i] - '0' + nTakeOver;
-        if (i == nLength - 1)
-            nSum++;
+void Print1ToMaxOfNDigits_1(int n) {
+  if (n <= 0) throw std::invalid_argument("");
+  char* number_str = new char[n + 1];
+  std::memset(number_str, '0', ((n + 1) * sizeof(char)));
+  number_str[n] = '\0';
 
-        if (nSum >= 10)
-        {
-            if (i == 0)
-                isOverflow = true;
-            else
-            {
-                nSum -= 10;
-                nTakeOver = 1;
-                number[i] = '0' + nSum;
-            }
-        }
-        else
-        {
-            number[i] = '0' + nSum;
-            break;
-        }
-    }
-
-    return isOverflow;
+  while (IncreaseNumber(number_str) == false) {
+    PrintNumber(number_str);
+  }
+  delete[] number_str;
+  return;
 }
 
 // ====================方法二====================
-void Print1ToMaxOfNDigits_2(int n)
-{
-    if (n <= 0)
-        return;
+void Print1ToMaxOfNDigits_Recursively(char* number_str, int length, int cur_idx) {
+  if ((number_str == nullptr) || (length <= 0) || (cur_idx < 0) || (cur_idx > length)) {
+    throw std::invalid_argument("");
+  }
 
-    char* number = new char[n + 1];
-    number[n] = '\0';
-
-    for (int i = 0; i < 10; ++i)
-    {
-        number[0] = i + '0';
-        Print1ToMaxOfNDigitsRecursively(number, n, 0);
-    }
-
-    delete[] number;
+  if (cur_idx == length) {
+    PrintNumber(number_str);
+    return;
+  }
+  for (int i = 0; i < 10; ++i) {
+    number_str[cur_idx] = '0' + i;
+    Print1ToMaxOfNDigits_Recursively(number_str, length, (cur_idx + 1));
+  }
+  return;
 }
 
-void Print1ToMaxOfNDigitsRecursively(char* number, int length, int index)
-{
-    if (index == length - 1)
-    {
-        PrintNumber(number);
-        return;
-    }
-
-    for (int i = 0; i < 10; ++i)
-    {
-        number[index + 1] = i + '0';
-        Print1ToMaxOfNDigitsRecursively(number, length, index + 1);
-    }
-}
-
-// ====================公共函数====================
-// 字符串number表示一个数字，数字有若干个0开头
-// 打印出这个数字，并忽略开头的0
-void PrintNumber(char* number)
-{
-    bool isBeginning0 = true;
-    int nLength = strlen(number);
-
-    for (int i = 0; i < nLength; ++i)
-    {
-        if (isBeginning0 && number[i] != '0')
-            isBeginning0 = false;
-
-        if (!isBeginning0)
-        {
-            printf("%c", number[i]);
-        }
-    }
-
-    printf("\t");
+void Print1ToMaxOfNDigits_2(int n) {
+  if (n <= 0) throw std::invalid_argument("");
+  char* number_str = new char[n + 1];
+  std::memset(number_str, 0, (n + 1) * sizeof(char));
+  Print1ToMaxOfNDigits_Recursively(number_str, n, 0);
+  return;
 }
 
 // ====================测试代码====================
-void Test(int n)
-{
-    printf("Test for %d begins:\n", n);
-
+void Test(int n, bool is_invalid_args) {
+  printf("Test for %d begins:\n", n);
+  try {
     Print1ToMaxOfNDigits_1(n);
     Print1ToMaxOfNDigits_2(n);
+  } catch (std::invalid_argument const& ia) {
+    if (is_invalid_args == true) {
+      printf("Passed.\n");
+    } else {
+      printf("Failed.\n");
+    }
+  }
 
-    printf("\nTest for %d ends.\n", n);
+  printf("\nTest for %d ends.\n", n);
 }
 
-int main(int argc, char* argv[])
-{
-    Test(1);
-    Test(2);
-    Test(3);
-    Test(0);
-    Test(-1);
+int main(int argc, char* argv[]) {
+  Test(1, false);
+  Test(2, false);
+  Test(3, false);
+  Test(0, true);
+  Test(-1, true);
 
-    return 0;
+  return 0;
 }
-

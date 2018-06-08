c0cb078c93554686900043ac4d5d0b7dca9125cb
Johnson Zhang <Johnson9009@163.com>
2018-06-08 14:26:41 +0800

    Practice 29_PrintMatrix

--- 29_PrintMatrix/PrintMatrix.cpp
+++ 29_PrintMatrix/PrintMatrix.cpp
@@ -8,200 +8,156 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 *******************************************************************/
 
 //==================================================================
-// 《剑指Offer――名企面试官精讲典型编程题》代码
-// 作者：何海涛
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+// 作者: 何海涛
 //==================================================================
 
-// 面试题29：顺时针打印矩阵
-// 题目：输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字。
+// 面试题29: 顺时针打印矩阵
+// 题目: 输入一个矩阵, 按照从外向里以顺时针的顺序依次打印出每一个数字.
 
 #include <cstdio>
+#include <stdexcept>
 
-void PrintMatrixInCircle(int** numbers, int columns, int rows, int start);
-void printNumber(int number);
+void PrintMatrixClockwisely(int* matrix, int row_cnt, int col_cnt) {
+  if ((matrix == nullptr) || (row_cnt <= 0) || (col_cnt <= 0)) throw std::invalid_argument("");
+  int row_first_idx = 0;
+  int row_last_idx = row_cnt - 1;
+  int col_first_idx = 0;
+  int col_last_idx = col_cnt - 1;
 
-void PrintMatrixClockwisely(int** numbers, int columns, int rows)
-{
-    if(numbers == nullptr || columns <= 0 || rows <= 0)
-        return;
-
-    int start = 0;
-
-    while(columns > start * 2 && rows > start * 2)
-    {
-        PrintMatrixInCircle(numbers, columns, rows, start);
-
-        ++start;
+  while ((row_first_idx <= row_last_idx) && (col_first_idx <= col_last_idx)) {
+    for (int i = col_first_idx; i <= col_last_idx; ++i) {
+      printf("%d ", matrix[row_first_idx * col_cnt + i]);
     }
-}
+    ++row_first_idx;
 
-void PrintMatrixInCircle(int** numbers, int columns, int rows, int start)
-{
-    int endX = columns - 1 - start;
-    int endY = rows - 1 - start;
+    for (int i = row_first_idx; i <= row_last_idx; ++i) {
+      printf("%d ", matrix[i * col_cnt + col_last_idx]);
+    }
+    --col_last_idx;
 
-    // 从左到右打印一行
-    for(int i = start; i <= endX; ++i)
-    {
-        int number = numbers[start][i];
-        printNumber(number);
+    if (row_first_idx <= row_last_idx) {
+      for (int i = col_last_idx; i >= col_first_idx; --i) {
+        printf("%d ", matrix[row_last_idx * col_cnt + i]);
+      }
+      --row_last_idx;
     }
 
-    // 从上到下打印一列
-    if(start < endY)
-    {
-        for(int i = start + 1; i <= endY; ++i)
-        {
-            int number = numbers[i][endX];
-            printNumber(number);
-        }
+    if (col_first_idx <= col_last_idx) {
+      for (int i = row_last_idx; i >= row_first_idx; --i) {
+        printf("%d ", matrix[i * col_cnt + col_first_idx]);
+      }
+      ++col_first_idx;
     }
-
-    // 从右到左打印一行
-    if(start < endX && start < endY)
-    {
-        for(int i = endX - 1; i >= start; --i)
-        {
-            int number = numbers[endY][i];
-            printNumber(number);
-        }
-    }
-
-    // 从下到上打印一行
-    if(start < endX && start < endY - 1)
-    {
-        for(int i = endY - 1; i >= start + 1; --i)
-        {
-            int number = numbers[i][start];
-            printNumber(number);
-        }
-    }
-}
-
-void printNumber(int number)
-{
-    printf("%d\t", number);
+  }
+  return;
 }
 
 // ====================测试代码====================
-void Test(int columns, int rows)
-{
-    printf("Test Begin: %d columns, %d rows.\n", columns, rows);
+void Test(int columns, int rows) {
+  printf("Test Begin: %d columns, %d rows.\n", columns, rows);
 
-    if(columns < 1 || rows < 1)
-        return;
+  if (columns < 1 || rows < 1) return;
 
-    int** numbers = new int*[rows];
-    for(int i = 0; i < rows; ++i)
-    {
-        numbers[i] = new int[columns];
-        for(int j = 0; j < columns; ++j)
-        {
-            numbers[i][j] = i * columns + j + 1;
-        }
-    }
+  int* numbers = new int[rows * columns];
+  for (int i = 0; i < (rows * columns); ++i) numbers[i] = i + 1;
 
-    PrintMatrixClockwisely(numbers, columns, rows);
-    printf("\n");
+  PrintMatrixClockwisely(numbers, rows, columns);
+  printf("\n");
 
-    for(int i = 0; i < rows; ++i)
-        delete[] (int*)numbers[i];
-
-    delete[] numbers;
+  delete[] numbers;
 }
 
-int main(int argc, char* argv[])
-{
-    /*
-    1    
-    */
-    Test(1, 1);
+int main(int argc, char* argv[]) {
+  /*
+  1
+  */
+  Test(1, 1);
 
-    /*
-    1    2
-    3    4
-    */
-    Test(2, 2);
+  /*
+  1    2
+  3    4
+  */
+  Test(2, 2);
 
-    /*
-    1    2    3    4
-    5    6    7    8
-    9    10   11   12
-    13   14   15   16
-    */
-    Test(4, 4);
+  /*
+  1    2    3    4
+  5    6    7    8
+  9    10   11   12
+  13   14   15   16
+  */
+  Test(4, 4);
 
-    /*
-    1    2    3    4    5
-    6    7    8    9    10
-    11   12   13   14   15
-    16   17   18   19   20
-    21   22   23   24   25
-    */
-    Test(5, 5);
+  /*
+  1    2    3    4    5
+  6    7    8    9    10
+  11   12   13   14   15
+  16   17   18   19   20
+  21   22   23   24   25
+  */
+  Test(5, 5);
 
-    /*
-    1
-    2
-    3
-    4
-    5
-    */
-    Test(1, 5);
+  /*
+  1
+  2
+  3
+  4
+  5
+  */
+  Test(1, 5);
 
-    /*
-    1    2
-    3    4
-    5    6
-    7    8
-    9    10
-    */
-    Test(2, 5);
+  /*
+  1    2
+  3    4
+  5    6
+  7    8
+  9    10
+  */
+  Test(2, 5);
 
-    /*
-    1    2    3
-    4    5    6
-    7    8    9
-    10   11   12
-    13   14   15
-    */
-    Test(3, 5);
+  /*
+  1    2    3
+  4    5    6
+  7    8    9
+  10   11   12
+  13   14   15
+  */
+  Test(3, 5);
 
-    /*
-    1    2    3    4
-    5    6    7    8
-    9    10   11   12
-    13   14   15   16
-    17   18   19   20
-    */
-    Test(4, 5);
+  /*
+  1    2    3    4
+  5    6    7    8
+  9    10   11   12
+  13   14   15   16
+  17   18   19   20
+  */
+  Test(4, 5);
 
-    /*
-    1    2    3    4    5
-    */
-    Test(5, 1);
+  /*
+  1    2    3    4    5
+  */
+  Test(5, 1);
 
-    /*
-    1    2    3    4    5
-    6    7    8    9    10
-    */
-    Test(5, 2);
+  /*
+  1    2    3    4    5
+  6    7    8    9    10
+  */
+  Test(5, 2);
 
-    /*
-    1    2    3    4    5
-    6    7    8    9    10
-    11   12   13   14    15
-    */
-    Test(5, 3);
+  /*
+  1    2    3    4    5
+  6    7    8    9    10
+  11   12   13   14    15
+  */
+  Test(5, 3);
 
-    /*
-    1    2    3    4    5
-    6    7    8    9    10
-    11   12   13   14   15
-    16   17   18   19   20
-    */
-    Test(5, 4);
+  /*
+  1    2    3    4    5
+  6    7    8    9    10
+  11   12   13   14   15
+  16   17   18   19   20
+  */
+  Test(5, 4);
 
-    return 0;
+  return 0;
 }
-

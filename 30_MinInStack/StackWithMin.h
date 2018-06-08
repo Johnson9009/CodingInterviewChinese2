1b57928b9a6769e0745c54d9ac8a184ac2990aef
Johnson Zhang <Johnson9009@163.com>
2018-06-08 21:35:27 +0800

    Practice 30_MinInStack

--- 30_MinInStack/StackWithMin.h
+++ 30_MinInStack/StackWithMin.h
@@ -1,84 +1,71 @@
 //==================================================================
-// 《剑指Offer――名企面试官精讲典型编程题》代码
-// 作者：何海涛
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+// 作者: 何海涛
 //==================================================================
 
-// 面试题30：包含min函数的栈
-// 题目：定义栈的数据结构，请在该类型中实现一个能够得到栈的最小元素的min
-// 函数。在该栈中，调用min、push及pop的时间复杂度都是O(1)。
+// 面试题30: 包含min函数的栈
+// 题目: 定义栈的数据结构, 请在该类型中实现一个能够得到栈的最小元素的min函数. 在该栈中, 调用min,
+// push及pop的时间复杂度都是O(1).
 
 #pragma once
 
-#include <stack>
 #include <assert.h>
+#include <stack>
 
-template <typename T> class StackWithMin
-{
-public:
-    StackWithMin() {}
-    virtual ~StackWithMin() {}
+template <typename T>
+class StackWithMin {
+ public:
+  T& top();
+  T const& top() const;
+  void pop();
+  void push(T const& item);
+  T const& min() const;
+  bool empty() const;
+  std::size_t size() const;
 
-    T& top();
-    const T& top() const;
-
-    void push(const T& value);
-    void pop();
-
-    const T& min() const;
-
-    bool empty() const;
-    size_t size() const;
-
-private:
-    std::stack<T>   m_data;     // 数据栈，存放栈的所有元素
-    std::stack<T>   m_min;      // 辅助栈，存放栈的最小元素
+ private:
+  std::stack<T> data_;
+  std::stack<T> min_stack_;
 };
 
-template <typename T> void StackWithMin<T>::push(const T& value)
-{
-    // 把新元素添加到辅助栈
-    m_data.push(value);
-
-    // 当新元素比之前的最小元素小时，把新元素插入辅助栈里；
-    // 否则把之前的最小元素重复插入辅助栈里
-    if(m_min.size() == 0 || value < m_min.top())
-        m_min.push(value);
-    else
-        m_min.push(m_min.top());
+template <typename T>
+T& StackWithMin<T>::top() {
+  return data_.top();
 }
 
-template <typename T> void StackWithMin<T>::pop()
-{
-    assert(m_data.size() > 0 && m_min.size() > 0);
-
-    m_data.pop();
-    m_min.pop();
+template <typename T>
+T const& StackWithMin<T>::top() const {
+  return data_.top();
 }
 
-
-template <typename T> const T& StackWithMin<T>::min() const
-{
-    assert(m_data.size() > 0 && m_min.size() > 0);
-
-    return m_min.top();
+template <typename T>
+void StackWithMin<T>::pop() {
+  data_.pop();
+  min_stack_.pop();
+  return;
 }
 
-template <typename T> T& StackWithMin<T>::top()
-{
-    return m_data.top();
+template <typename T>
+void StackWithMin<T>::push(T const& item) {
+  data_.push(item);
+  if ((min_stack_.empty() == false) && (item > min_stack_.top())) {
+    min_stack_.push(min_stack_.top());
+  } else {
+    min_stack_.push(item);
+  }
 }
 
-template <typename T> const T& StackWithMin<T>::top() const
-{
-    return m_data.top();
+template <typename T>
+T const& StackWithMin<T>::min() const {
+  return min_stack_.top();
 }
 
-template <typename T> bool StackWithMin<T>::empty() const
-{
-    return m_data.empty();
+template <typename T>
+bool StackWithMin<T>::empty() const {
+  return data_.empty();
 }
 
-template <typename T> size_t StackWithMin<T>::size() const
-{
-    return m_data.size();
+template <typename T>
+std::size_t StackWithMin<T>::size() const {
+  return data_.size();
 }

fe6b9565bb577108fb3933ecfa8c95800fade834
Johnson Zhang <Johnson9009@163.com>
2018-06-09 14:11:22 +0800

    Practice 33_SquenceOfBST

--- 33_SquenceOfBST/SquenceOfBST.cpp
+++ 33_SquenceOfBST/SquenceOfBST.cpp
@@ -8,74 +8,64 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 *******************************************************************/
 
 //==================================================================
-// 《剑指Offer――名企面试官精讲典型编程题》代码
-// 作者：何海涛
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+// 作者: 何海涛
 //==================================================================
 
 // 面试题33：二叉搜索树的后序遍历序列
-// 题目：输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。
-// 如果是则返回true，否则返回false。假设输入的数组的任意两个数字都互不相同。
+// 题目: 输入一个整数数组, 判断该数组是不是某二叉搜索树的后序遍历的结果. 如果是则返回true, 否则返回
+// false. 假设输入的数组的任意两个数字都互不相同.
 
 #include <cstdio>
+#include <stdexcept>
 
-// BST：Binary Search Tree，二叉搜索树
-bool VerifySquenceOfBST(int sequence[], int length)
-{
-    if(sequence == nullptr || length <= 0)
-        return false;
+bool IsValidPostOrderOfBstCore(int const* post_order, int begin, int end) {
+  if ((post_order == nullptr) || (begin > end)) throw std::invalid_argument("");
+  if ((begin + 1) >= end) return true;
 
-    int root = sequence[length - 1];
+  int root_value = post_order[end - 1];
+  int idx = begin;
+  while (post_order[idx] < root_value) ++idx;
+  for (int i = idx; i < end; ++i) {
+    if (post_order[i] < root_value) return false;
+  }
+  // NOTE: Must be "(end - 1)" can't be "end";
+  return (IsValidPostOrderOfBstCore(post_order, begin, idx) &&
+          IsValidPostOrderOfBstCore(post_order, idx, (end - 1)));
+}
 
-    // 在二叉搜索树中左子树的结点小于根结点
-    int i = 0;
-    for(; i < length - 1; ++ i)
-    {
-        if(sequence[i] > root)
-            break;
-    }
-
-    // 在二叉搜索树中右子树的结点大于根结点
-    int j = i;
-    for(; j < length - 1; ++ j)
-    {
-        if(sequence[j] < root)
-            return false;
-    }
-
-    // 判断左子树是不是二叉搜索树
-    bool left = true;
-    if(i > 0)
-        left = VerifySquenceOfBST(sequence, i);
-
-    // 判断右子树是不是二叉搜索树
-    bool right = true;
-    if(i < length - 1)
-        right = VerifySquenceOfBST(sequence + i, length - i - 1);
-
-    return (left && right);
+bool IsValidPostOrderOfBst(int const* post_order, int length) {
+  if ((post_order == nullptr) || (length <= 0)) throw std::invalid_argument("");
+  return IsValidPostOrderOfBstCore(post_order, 0, length);
 }
 
 // ====================测试代码====================
-void Test(const char* testName, int sequence[], int length, bool expected)
-{
-    if(testName != nullptr)
-        printf("%s begins: ", testName);
+void Test(const char* testName, int sequence[], int length, bool expected) {
+  if (testName != nullptr) printf("%s begins: ", testName);
 
-    if(VerifySquenceOfBST(sequence, length) == expected)
-        printf("passed.\n");
-    else
-        printf("failed.\n");
+  try {
+    if (IsValidPostOrderOfBst(sequence, length) == expected) {
+      printf("passed.\n");
+    } else {
+      printf("failed.\n");
+    }
+  } catch (std::invalid_argument const& ia) {
+    if (expected == false) {
+      printf("passed.\n");
+    } else {
+      printf("failed.\n");
+    }
+  }
 }
 
-//            10
-//         /      \
-//        6        14
-//       /\        /\
-//      4  8     12  16
-void Test1()
-{
-    int data[] = {4, 8, 6, 12, 16, 14, 10};
-    Test("Test1", data, sizeof(data)/sizeof(int), true);
+//          10
+//         /  \
+//        6    14
+//       / \   / \
+//      4   8 12 16
+void Test1() {
+  int data[] = {4, 8, 6, 12, 16, 14, 10};
+  Test("Test1", data, sizeof(data) / sizeof(int), true);
 }
 
 //           5
@@ -83,10 +73,9 @@ void Test1()
 //         4   7
 //            /
 //           6
-void Test2()
-{
-    int data[] = {4, 6, 7, 5};
-    Test("Test2", data, sizeof(data)/sizeof(int), true);
+void Test2() {
+  int data[] = {4, 6, 7, 5};
+  Test("Test2", data, sizeof(data) / sizeof(int), true);
 }
 
 //               5
@@ -98,10 +87,9 @@ void Test2()
 //         2
 //        /
 //       1
-void Test3()
-{
-    int data[] = {1, 2, 3, 4, 5};
-    Test("Test3", data, sizeof(data)/sizeof(int), true);
+void Test3() {
+  int data[] = {1, 2, 3, 4, 5};
+  Test("Test3", data, sizeof(data) / sizeof(int), true);
 }
 
 // 1
@@ -113,47 +101,40 @@ void Test3()
 //       4
 //        \
 //         5
-void Test4()
-{
-    int data[] = {5, 4, 3, 2, 1};
-    Test("Test4", data, sizeof(data)/sizeof(int), true);
+void Test4() {
+  int data[] = {5, 4, 3, 2, 1};
+  Test("Test4", data, sizeof(data) / sizeof(int), true);
 }
 
 // 树中只有1个结点
-void Test5()
-{
-    int data[] = {5};
-    Test("Test5", data, sizeof(data)/sizeof(int), true);
+void Test5() {
+  int data[] = {5};
+  Test("Test5", data, sizeof(data) / sizeof(int), true);
 }
 
-void Test6()
-{
-    int data[] = {7, 4, 6, 5};
-    Test("Test6", data, sizeof(data)/sizeof(int), false);
+void Test6() {
+  int data[] = {7, 4, 6, 5};
+  Test("Test6", data, sizeof(data) / sizeof(int), false);
 }
 
-void Test7()
-{
-    int data[] = {4, 6, 12, 8, 16, 14, 10};
-    Test("Test7", data, sizeof(data)/sizeof(int), false);
+void Test7() {
+  int data[] = {4, 6, 12, 8, 16, 14, 10};
+  Test("Test7", data, sizeof(data) / sizeof(int), false);
 }
 
-void Test8()
-{
-    Test("Test8", nullptr, 0, false);
+void Test8() {
+  Test("Test8", nullptr, 0, false);
 }
 
-int main(int argc, char* argv[])
-{
-    Test1();
-    Test2();
-    Test3();
-    Test4();
-    Test5();
-    Test6();
-    Test7();
-    Test8();
+int main(int argc, char* argv[]) {
+  Test1();
+  Test2();
+  Test3();
+  Test4();
+  Test5();
+  Test6();
+  Test7();
+  Test8();
 
-    return 0;
+  return 0;
 }
-

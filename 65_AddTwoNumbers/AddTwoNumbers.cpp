5e3e019f9ad1781743d370e359a51adbee6f07cf
Johnson Zhang <Johnson9009@163.com>
2018-06-15 00:24:26 +0800

    Practice 65_AddTwoNumbers

--- 65_AddTwoNumbers/AddTwoNumbers.cpp
+++ 65_AddTwoNumbers/AddTwoNumbers.cpp
@@ -8,55 +8,46 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 *******************************************************************/
 
 //==================================================================
-// 《剑指Offer――名企面试官精讲典型编程题》代码
-// 作者：何海涛
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+// 作者: 何海涛
 //==================================================================
 
-// 面试题65：不用加减乘除做加法
-// 题目：写一个函数，求两个整数之和，要求在函数体内不得使用＋、－、×、÷
-// 四则运算符号。
+// 面试题65: 不用加减乘除做加法
+// 题目: 写一个函数, 求两个整数之和, 要求在函数体内不得使用+, -, *、/四则运算符号.
 
 #include <cstdio>
 
-int Add(int num1, int num2)
-{
-    int sum, carry;
-    do
-    {
-        sum = num1 ^ num2;
-        carry = (num1 & num2) << 1;
-
-        num1 = sum;
-        num2 = carry;
-    }
-    while(num2 != 0);
-
-    return num1;
+int Add(int number1, int number2) {
+  int sum = number1 ^ number2;
+  int carry = (number1 & number2) << 1;
+  while (carry != 0) {
+    int tmp = sum;
+    sum ^= carry;
+    carry = (carry & tmp) << 1;
+  }
+  return sum;
 }
 
 // ====================测试代码====================
-void Test(int num1, int num2, int expected)
-{
-    int result = Add(num1, num2);
-    if(result == expected)
-        printf("%d + %d is %d. Passed\n", num1, num2, result);
-    else
-        printf("%d + %d is %d. FAILED\n", num1, num2, result);
+void Test(int num1, int num2, int expected) {
+  int result = Add(num1, num2);
+  if (result == expected)
+    printf("%d + %d is %d. Passed\n", num1, num2, result);
+  else
+    printf("%d + %d is %d. FAILED\n", num1, num2, result);
 }
 
-int main(int argc, char* argv[])
-{
-    Test(1, 2, 3);
-    Test(111, 899, 1010);
+int main(int argc, char* argv[]) {
+  Test(1, 2, 3);
+  Test(111, 899, 1010);
 
-    Test(-1, 2, 1);
-    Test(1, -2, -1);
+  Test(-1, 2, 1);
+  Test(1, -2, -1);
 
-    Test(3, 0, 3);
-    Test(0, -4, -4);
+  Test(3, 0, 3);
+  Test(0, -4, -4);
 
-    Test(-2, -8, -10);
+  Test(-2, -8, -10);
 
-    return 0;
+  return 0;
 }
-
